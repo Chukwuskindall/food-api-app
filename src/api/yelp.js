fa22ae2e11e8d67f6c680679f5745798807dae0e
Chukwuskindall <nwachukwuemy@gmail.com>
2024-06-06 20:09:25 +0500

    Update yelp.js

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -4,7 +4,7 @@ export default axios.create({
   baseURL: "https://api.yelp.com/v3/businesses",
   headers: {
     Authorization:
-      "Bearer mlotp3GIFZf-YmgzjOfCbILsGVKnmZdNSTfH7mK0Ii4W6wGyTUrsWOhnLFRZeIVCdCpYFuRndXNqLt0svTBWwy5mnPANLuUl8IZeiFT_06_sXeTSEGx",
+      "Bearer ",
   },
 });
 
